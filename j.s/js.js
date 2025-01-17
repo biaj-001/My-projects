a784a793d1a71ead2a0de29c3d97fbee0f66521e
biaj_001 <acetynne@gmail.com>
2025-01-17 23:42:55 +0300

    added initial files to repo

---
+++ j.s/js.js
@@ -0,0 +1,160 @@
+// alert("Start now!!!")
+//  print("ok")
+
+// let x=10;
+// console.log(x+1)
+// console.log(x*5)
+// console.log(typeof 1)
+// console.log(typeof 'stackies')
+// let y=1;
+// console.log(y+x)
+// let abstr="i am ace"
+// console.log(abstr.length)
+// let bcstr=abstr.replace('ace','tom')
+// console.log(bcstr)
+
+// for (i=1; i<6; i++){
+//    console.log(i)
+// }
+
+// for (i=0;i<10;i++){
+//     if (i==6){
+//         break;
+//     }
+//     console.log('Total:' + i);
+// }
+
+// for ( i=1; i<6; i+=2 ){
+//     if (i==5) {
+//         break;
+//     }
+//     console.log('Target no:' + i)
+// }
+// const city= {
+//     a: "Nairobi",
+//     b: "Mombasa",
+//     c: "Naivasha"
+// }
+// for ( let key in city){
+//     console.log(key + ":"+ city[key])
+// }
+
+// console.log('What is the answer of 5 * 3 ?')
+// const math = {
+//     a: 12,
+//     b: 16,
+//     c: 15,
+//     d: 20
+
+// }
+// for (let key in math){
+//     console.log(key +":"+ math[key])
+// }
+
+// const arr = [1,2,3,4,];
+
+// for ( const i in arr){
+//     console.log(i)
+// }
+// let i = 0;
+// while(i<9){
+//     console.log(i)
+//     i++
+// }
+// while
+
+// let i = 0
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while(i<10);
+// do ....while
+
+// let age= 19;
+
+// if ( age > 18){
+//     console.log("Nick is an adult")
+// }
+// else{
+//     console.log("nick is a child")
+// }
+// const pet = "dog";
+
+// const music= "rap"
+
+// switch (music){
+//     case "gospel":
+//         console.log("i love gospel music");
+//         break;
+//     case "electric":
+//         console.log("i love electric music");
+//         break;
+//     case "reggae":
+//         console.log("i love reggae music");
+//         break;
+        
+//     case "rap":
+//          console.log("i love rap music");
+//          break;
+
+//     default:
+//         console.log("i love all types of music");
+//         break
+// }
+// const country = "Ireland";
+
+// switch (country) {
+
+//      case "France":
+//      case "Spain":
+//      case "Ireland":
+//      case "Poland":
+//           console.log("This country is in Europe.");
+//           break;
+//      case "United States":
+//      default:
+//          console.log("This country is not in Europe.");
+// }
+
+// const city = "mombasa"
+
+// switch (city){
+//     case "nairobi":
+//     case "naivasha":
+//     case "mombasa":
+//     case "nakuru":
+//         console.log("The city is in kenya");
+//         break;
+//     case "dubai":
+//     default:
+//         console.log("This city is not i kenya")
+// }
+
+// function showmessage(){
+//     alert("hello");
+
+// }
+// showmessage()
+
+// function showmessage(){
+//     let message = "Transaction";
+//     alert(message)
+// }
+// showmessage()
+// alert(message)
+
+// let username ="done"
+
+// function showmessage(){
+//     let message = " hello" + username
+//     alert(message)
+
+// }
+// showmessage()
+
+// function showmessage( from , text){
+//     alert (from + ":" + text);
+// }
+// showmessage("anne",'hello')
+// showmessage("anne","what is you")
